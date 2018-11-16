abc2549ecf51495f1879b0ca3baeb0fc786c2c67
Hedya Febritanti <hedya.feb@gmail.com>
2018-11-16 10:08:01 +0700

    addLogin

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,53 +1,91 @@
 <template>
-  <div class="home">
-    <form>
-      <input type="text" v-model="userID" placeholder="userID">
-      <input type="text" v-model="email">
-      <button @click.prevent="testWrite"> submit</button>
-      {{ allPlayers }}
-    </form>
-  </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-2">
+
+            </div>
+
+            <div class="col-8">
+                <h1>Welcome to room {{ room }}</h1>
+                <form class="form-inline my-5">
+                    <div class="form-group mb-2">
+                        <input type="text" class="form-control-plaintext" placeholder="input player name" v-model="username">
+                    </div>
+                    <button type="submit" class="btn btn-primary mb-2" @click.prevent="addPlayer">Add Player</button>
+                </form>
+                <br><br>
+                {{ alert }}
+
+                <p>Current Player: </p>
+                <ul>
+                    <li v-for="player in allPlayers" style="list-style:none"> {{ player.username }}</li>
+                </ul>
+            </div>
+
+            <div class="col-2">
+
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import database from '@/assets/config.js';
-import HelloWorld from '@/components/HelloWorld.vue'
 
 const db = database
 
 export default {
   data() {
     return {
-      userID: "",
-      email: "",
-      allPlayers: []
+      username: "",
+      room: "",
+      allPlayers: [],
+      alert: ""
     }
   },
-  methods: {
-    // testWrite: function () {
-    //     db.ref('/db/room1/' + this.userID).push({
-    //     email: this.email
-    //     }, function(err) {
-    //       if (err) {
-    //         console.log(err);
-    //       }
-    //       else {
-    //         console.log('data saved successfully');
-    //       }
-    //     })
-    // },
-    testRead: function () {
-      db.ref('/room1').on('value', (snapshot) => {
-        if (snapshot.val()) {
-          console.log(snapshot.val());
-          this.allPlayers = snapshot.val()
+    methods: {
+        addPlayer: function () {
+            let self = this
+            if (self.username == '') {
+                this.alert = 'Please enter your name'
+            }
+            else {
+                this.alert = ''
+                db.ref('room/' + self.room + '/users').push({
+                    username: self.username
+                })
+                .then( response => {
+                    localStorage.setItem('userID', response.key)
+                    self.getPlayer()
+                })
+                self.username = ''
+            }
+        },
+        checkRoom() {
+            let roomValue = localStorage.getItem('room')
+            this.room = roomValue
+        },
+
+        getPlayer: function() {
+            let self = this
+            let roomName = localStorage.getItem('room')
+            console.log('this is room name', roomName);
+            
+            db.ref('room/' + roomName + '/users').once('value', function (snapshot) {
+                let result = snapshot.val()
+                self.allPlayers = Object.values(result)
+                console.log(self.allPlayers);
+            })
         }
-      })
+
+    },
+
+
+    created() {
+        this.checkRoom()
+        this.getPlayer()
     }
-  },
-  mounted() {
-    this.testRead()
-  }
+
 }
 </script>
