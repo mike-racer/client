6914031b656630255a88db8b0f59ec0bc8c2fcfd
Hedya Febritanti <hedya.feb@gmail.com>
2018-11-16 14:37:35 +0700

    add router push

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,24 +1,25 @@
 <template>
     <div class="container mt-5">
         <div class="row">
-            <div class="col-2">
-
+            <div class="col-2 border">
+                <img height="100" width="100" src="https://s4.bukalapak.com/img/969394342/w-1000/Mainan_Kuda_Kudaan_Tunggang_Karet_dengan_Music_Bunyi_dan_Lampu_Promo.jpg.webp" alt="">
             </div>
 
             <div class="col-8">
-                <h1>Welcome to room {{ room }}</h1>
-                <form class="form-inline my-5">
-                    <div class="form-group mb-2">
-                        <input type="text" class="form-control-plaintext" placeholder="input player name" v-model="username">
+                <h1>Welcome to room "{{ room }}" </h1>
+                <form class="form-inline border">
+                    <div class="form-group">
+                        <input type="text" class="form-control-plaintext border" placeholder="input player name" v-model="username">
+                        <button type="submit" class="btn btn-primary border" @click.prevent="addPlayer">Add Player</button>
                     </div>
-                    <button type="submit" class="btn btn-primary mb-2" @click.prevent="addPlayer">Add Player</button>
                 </form>
                 <br><br>
                 {{ alert }}
 
+
                 <p>Current Player: </p>
                 <ul>
-                    <li v-for="player in allPlayers" style="list-style:none"> {{ player.username }}</li>
+                    <li v-for="player in allPlayers" style="list-style:none" > {{ player.username }}</li>
                 </ul>
             </div>
 
@@ -57,7 +58,7 @@ export default {
                 })
                 .then( response => {
                     localStorage.setItem('userID', response.key)
-                    self.getPlayer()
+                    self.$router.push('/room') // add 1 line
                 })
                 self.username = ''
             }
