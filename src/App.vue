86dfa84b594145e54b808907ebf65eabd3955b33
Yaya Hidayatullah <yayahidayatullah@Yayas-MBP.localdomain>
2018-11-16 14:23:30 +0700

    add homepage

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,20 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    
   </div>
 </template>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+} */
+.footer{
+  margin-left: 200px;
 }
 #nav {
   padding: 30px;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <h3>MIKE RACER</h3>
+  <div class="home container">
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <div class="row">
+      <div class="col-2">
 
-    Room With Player :
-    <ul>
-      <li v-for="room in listRoom" :key="room.id"></li>
-    </ul>
-    Insert Room Name :
-    <input type="text" v-model="list"/>
-    <button @click="setRoom">Play</button>
-     </div>
+      </div>
+      <div class="col-8 border">
+        <h3>MIKE RACER</h3>
+        Insert Room Name :
+        <input type="text" v-model="room"/>
+        <button @click="setRoom">Join Room</button>
+        <br>
+        Room With Player :
+        <list :roomList="roomList" v-on:sender= "joinRoom($event)"/>
+        <img class="footer" src="http://showgirlequestrian.com.au/wp-content/uploads/2016/02/Horses-Running.png">
+      </div>
+      <div class="col-2">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 import database from '@/assets/config.js'
+import list from '@/components/listRoom.vue'
+import buttons from '@/components/button.vue'
 
 export default {
   name: 'home',
@@ -29,14 +38,16 @@ export default {
     }
   },
   components: {
-    HelloWorld
+    HelloWorld,
+    list,
+    buttons
   },
   methods: {
     setRoom(){
       let self = this
       if (self.room) {
         let racearena = database.ref('room/'+ self.room)
-        racearena.once('value', function(snapshoot){
+        racearena.once('value', function(snapshot){
           let result = snapshot.val()
           console.log('check room', result)
           if (!result) {
@@ -67,8 +78,28 @@ export default {
 
     getAllRoom(){
       let self = this
-      database.ref('room/').once('value', function(snapshoot){})
+      database.ref('room/').once('value', function(snapshoot){
+        console.log('get all room', snapshoot.val())
+        let result = snapshoot.val()
+        self.roomList = Object.keys(result)
+      })
+    },
+
+    joinRoom(room){
+      this.room = room
+     this.setRoom()
     }
+  },
+
+  created(){
+    this.getAllRoom()
   }
 }
 </script>
+<style scoped>
+.iniclass{
+  background: red;
+  border-color: red
+}
+</style>
+
